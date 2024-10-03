bdf83b98526f40e06e3a6f211e104bf1de2de89b
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-10-03 12:47:24 +0300

    Features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
 
   )
