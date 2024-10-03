065138adf00d9bc309d2283d47b66f4c9566db06
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-10-03 14:00:23 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
+
 const Footer = () => {
   return (
-    <div>
-      
+<footer className='py-5 sm:px-10 px-5 '>
+<div className="screen-max-width">
+        <div>
+          <p className="font-semibold text-gray text-xs text-center">
+            More ways to shop: {' '}
+            <span className="underline text-blue">
+            Find an Apple Store {' '}
+            </span>
+            or {' '}
+            <span className="underline text-blue">
+            other retailer
+            </span>{' '}
+            near you.
+            <span> {' '}  Or call 000800-040-1966 {' '}</span>
+            <span>{' '} Copright @ 2024 Apple Inc. All rights reserved. {' '}</span>
+          </p>
+         
+           
+        
+        </div>
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+        
+
     </div>
+</footer>
   )
 }
 
